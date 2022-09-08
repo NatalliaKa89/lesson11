7363bcaac8aa12033939829e13720f34810d278d
Matuk247 <artyomnikolenko4133@gmail.com>
2022-09-08 11:57:48 +0300

    updated mood detector program with const fields

--- src/Task07.java
+++ src/Task07.java
@@ -2,35 +2,43 @@
 // :( - bad
 // :/ - neutral
 // :() - shouted
-// :\ - nervous
+// :? - nervous
 
 import java.util.Random;
 
 
 public class Task07 {
-    public static void main(String[] args) {
 
+    public static  final int MOOD_NUMBERS = 5;
+
+    public static final String HAPPY_MOOD = ":)";
+    public static final String BAD_MOOD = ":(";
+    public static final String NEUTRAL_MOOD = ":/";
+    public static final String SHOUTED_MOOD = ":()";
+    public static final String NERVOUS_MOOD = ":?";
+
+
+    public static void main(String[] args) {
         Random random = new Random();
 
-        int mood = random.nextInt(5);
 
-        String msg = ":)";
+        int mood = random.nextInt(MOOD_NUMBERS);
+
+        String msg = HAPPY_MOOD;
 
         switch (mood) {
-            case 0:
-                msg = ":)";
-                break;
+
             case 1:
-                msg = ":(";
+                msg = BAD_MOOD;
                 break;
             case 2:
-                msg = ":|";
+                msg = NEUTRAL_MOOD;
                 break;
             case 3:
-                msg = ":()";
+                msg = SHOUTED_MOOD;
                 break;
             case 4:
-                msg = ":?";
+                msg = NERVOUS_MOOD;
                 break;
         }
 
