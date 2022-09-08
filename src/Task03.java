4d0f41f4ddd8fffe530b9c51cd1220b41c175654
Matuk247 <artyomnikolenko4133@gmail.com>
2022-09-08 10:24:51 +0300

    using Random class for random floating point numbers

---
+++ src/Task03.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Task03 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        // random floating point numbers
+        //for (int i = 0; i < 10; i++) {
+        //  System.out.println(random.nextDouble() + " ");
+        //}
+
+        int bound = 100;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(random.nextDouble() * 100);
+        }
+    }
+}
